53fa1cca0e6662bef786ec8bfde8c806affb8b91
whstruts <whstruts@163.com>
2020-08-09 22:56:38 +0800

    whstruts 新增限购api

---
+++ src/api/api_purchase_manage.js
@@ -0,0 +1,35 @@
+/**
+ * Created by bootdo.
+ * 用户相关api
+ */
+import * as API from './'
+
+export default {
+  addTemporaryCommodity: params => {
+    return API.POST('/api-mallcenter/CommodityPurchaseManage/addTemporaryCommodity', params)
+  },
+  batchRemove: params => {
+    return API.DELETE('/api-mallcenter/CommodityPurchaseManage/batchRemove', params)
+  },
+  cancelAdd: params => {
+    return API.DELETE('/api-mallcenter/CommodityPurchaseManage/cancelAdd', params)
+  },
+  chooseAbleCommodityList: params => {
+    return API.GET('/api-mallcenter/CommodityPurchaseManage/chooseAbleCommodityList', params)
+  },
+  getAddTemporaryId: params => {
+    return API.GET('/api-mallcenter/CommodityPurchaseManage/getAddTemporaryId', params)
+  },
+  list: params => {
+    return API.GET('/api-mallcenter/CommodityPurchaseManage/list', params)
+  },
+  saveTemporary: params => {
+    return API.PUT('/api-mallcenter/CommodityPurchaseManage/saveTemporary', params)
+  },
+  selectAddTemporaryList: params => {
+    return API.GET('/api-mallcenter/CommodityPurchaseManage/selectAddTemporaryList', params)
+  },
+  update: params => {
+    return API.POST('/api-mallcenter/CommodityPurchaseManage/update', params)
+  },
+}
